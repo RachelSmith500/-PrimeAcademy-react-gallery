9c62c6d2a29fa1fb088274993ce2f1020bd23a43
RachelSmith500 <rachelsmith9721@gmail.com>
2020-10-24 19:06:08 -0500

    objects containing image paths added to gallery items

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: '1.png', description: 'Pikes Palace in Seattle.', likes: 0 }
+    // { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: '1.png', description: 'Pikes Palace in Seattle.', likes: 0 },
+    { id: 2, path: '2.png', description: 'Pikes Palace in Seattle 2.', likes: 0 },
+    { id: 3, path: '3.png', description: 'Pikes Palace in Seattle 3.', likes: 0 },
+    { id: 4, path: '4.png', description: 'Pikes Palace in Seattle 4.', likes: 0 },
+    { id: 5, path: '5.png', description: 'Antiquing with Trav', likes: 0 },
+    { id: 6, path: '6.png', description: 'Looking out over the bay.', likes: 0 },
+    { id: 7, path: '8.png', description: 'A dogo living the good life.', likes: 0 }
 ];
 
 module.exports = galleryItems;
